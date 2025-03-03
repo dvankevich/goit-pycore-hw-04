45f0b0ac4bd98c55c6e7b12de4cd559f7bdaa5d4
dvankevich <dvankevich@gmail.com>
2025-03-03 20:08:28 +0200

    task 03

--- task03.py
+++ task03.py
@@ -1,28 +1,50 @@
 import sys
+import os
 from colorama import Fore, Style
 from pathlib import PurePath, Path
 
 FILE_COLOR = Fore.GREEN
 DIR_COLOR = Fore.LIGHTBLUE_EX
+LINK_COLOR = Fore.LIGHTMAGENTA_EX
 MARGIN = "  "
 depth = 0 # directory depth value
 
 def color_print(str, color):
+    '''
+    Print string str with color
+    Args:
+        str (str): string to print
+        color: color constant from colorama library
+    '''
     print(color + str + Style.RESET_ALL)
 
 def help():
-    print("help message for script "+  PurePath(sys.argv[0]).name)
+    '''
+    Show help and usage message.
+    '''
+    print("lists all subdirectories and files in the specified directory using different colors and icons for subdirectories and files")
+    print("Usage:")
+    print(PurePath(sys.argv[0]).name + "dir_name")
 
 def dir_list(directory, deep):
+    '''
+    lists all subdirectories and files in the specified directory using different colors 
+    and icons for subdirectories and files.
+    Args:
+        directory (str): path to directory
+        deep (int): nesting depth - affects indentation
+    '''
     if deep == 0 and Path(directory).is_dir():
         color_print("📂" + str(directory), DIR_COLOR)
         deep += 1
 
     for path in directory.iterdir():
-        if Path(path).is_dir():
+        if os.path.islink(Path(path)):
+            color_print(MARGIN * deep + PurePath(path).name, LINK_COLOR)
+        elif Path(path).is_dir():
             color_print(MARGIN * deep + "📂" + PurePath(path).name, DIR_COLOR)
             deep += 1
-            path_to_dir = Path(directory) / PurePath(path).name # should create the correct path for Windows as well
+            path_to_dir = Path(directory).joinpath(PurePath(path).name) # should create the correct path for Windows as well
             #print(deep, path_to_dir)
             dir_list(path_to_dir, deep)
             deep -= 1
